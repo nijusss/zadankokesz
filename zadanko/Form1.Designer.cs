5babb53e844faa1cb130c0b9a69049492e0ca809
Jakub Pruchnicki <jakub.pruchnicki@teb.edu.pl>
2022-05-10 14:02:15 +0200

    ok

--- zadanko/Form1.Designer.cs
+++ zadanko/Form1.Designer.cs
@@ -22,10 +22,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.upgradeButton = new System.Windows.Forms.Button();
             this.ButtonLevelTextBox = new System.Windows.Forms.TextBox();
+            this.A1UpgradeAmmount = new System.Windows.Forms.Button();
+            this.A1UpgradeInterval = new System.Windows.Forms.Button();
+            this.A1Label = new System.Windows.Forms.Label();
+            this.A1ammountTextBox = new System.Windows.Forms.TextBox();
+            this.A1IntervalTextBox = new System.Windows.Forms.TextBox();
+            this.A1Timer = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -41,7 +50,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(639, 24);
+            this.label1.Location = new System.Drawing.Point(667, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 15);
             this.label1.TabIndex = 1;
@@ -65,17 +74,89 @@
             this.ButtonLevelTextBox.Size = new System.Drawing.Size(122, 23);
             this.ButtonLevelTextBox.TabIndex = 3;
             // 
+            // A1UpgradeAmmount
+            // 
+            this.A1UpgradeAmmount.Location = new System.Drawing.Point(208, 6);
+            this.A1UpgradeAmmount.Name = "A1UpgradeAmmount";
+            this.A1UpgradeAmmount.Size = new System.Drawing.Size(111, 44);
+            this.A1UpgradeAmmount.TabIndex = 4;
+            this.A1UpgradeAmmount.Text = "Ulepsz ilość";
+            this.A1UpgradeAmmount.UseVisualStyleBackColor = true;
+            this.A1UpgradeAmmount.Click += new System.EventHandler(this.A1UpgradeAmmount_Click);
+            // 
+            // A1UpgradeInterval
+            // 
+            this.A1UpgradeInterval.Location = new System.Drawing.Point(431, 6);
+            this.A1UpgradeInterval.Name = "A1UpgradeInterval";
+            this.A1UpgradeInterval.Size = new System.Drawing.Size(125, 44);
+            this.A1UpgradeInterval.TabIndex = 5;
+            this.A1UpgradeInterval.Text = "Ulepsz częstotliwość";
+            this.A1UpgradeInterval.UseVisualStyleBackColor = true;
+            this.A1UpgradeInterval.Click += new System.EventHandler(this.A1UpgradeInterval_Click);
+            // 
+            // A1Label
+            // 
+            this.A1Label.AutoSize = true;
+            this.A1Label.Location = new System.Drawing.Point(12, 19);
+            this.A1Label.Name = "A1Label";
+            this.A1Label.Size = new System.Drawing.Size(78, 15);
+            this.A1Label.TabIndex = 6;
+            this.A1Label.Text = "AutoUpgrade";
+            this.A1Label.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // A1ammountTextBox
+            // 
+            this.A1ammountTextBox.Location = new System.Drawing.Point(102, 16);
+            this.A1ammountTextBox.Name = "A1ammountTextBox";
+            this.A1ammountTextBox.ReadOnly = true;
+            this.A1ammountTextBox.Size = new System.Drawing.Size(100, 23);
+            this.A1ammountTextBox.TabIndex = 7;
+            this.A1ammountTextBox.Text = "0";
+            this.A1ammountTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.A1ammountTextBox.TextChanged += new System.EventHandler(this.A1LevelTextBox_TextChanged);
+            // 
+            // A1IntervalTextBox
+            // 
+            this.A1IntervalTextBox.Location = new System.Drawing.Point(325, 16);
+            this.A1IntervalTextBox.Name = "A1IntervalTextBox";
+            this.A1IntervalTextBox.Size = new System.Drawing.Size(100, 23);
+            this.A1IntervalTextBox.TabIndex = 8;
+            this.A1IntervalTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.A1IntervalTextBox.TextChanged += new System.EventHandler(this.A1IntervalTextBox_TextChanged);
+            // 
+            // A1Timer
+            // 
+            this.A1Timer.Tick += new System.EventHandler(this.A1Timer_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(357, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "/min";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(801, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.A1IntervalTextBox);
+            this.Controls.Add(this.A1ammountTextBox);
+            this.Controls.Add(this.A1Label);
+            this.Controls.Add(this.A1UpgradeInterval);
+            this.Controls.Add(this.A1UpgradeAmmount);
             this.Controls.Add(this.ButtonLevelTextBox);
             this.Controls.Add(this.upgradeButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,5 +168,12 @@
         private Label label1;
         private Button upgradeButton;
         private TextBox ButtonLevelTextBox;
+        private Button A1UpgradeAmmount;
+        private Button A1UpgradeInterval;
+        private Label A1Label;
+        private TextBox A1ammountTextBox;
+        private TextBox A1IntervalTextBox;
+        private System.Windows.Forms.Timer A1Timer;
+        private Label label2;
     }
 }
--- zadanko/Form1.cs
+++ zadanko/Form1.cs
@@ -2,30 +2,49 @@ namespace zadanko
 {
     public partial class Form1 : Form
     {
-        int cash;
+        private int cash;
+        public int Cash
+        {
+            set
+            {
+                cash = value;
+                label1.Text ="Kasa: $" + value.ToString();
+            }
+            get
+            { 
+                return cash; 
+            }
+        }
         int buttonLevel;
+        int A1Ammount;
+        int A1Interval;
+
         public Form1()
         {
             InitializeComponent();
-            cash = 0;
+            Cash = 0;
             buttonLevel = 1;
+            A1Ammount = 10;
+            A1Interval = 0;
+            A1ammountTextBox.Text = A1Ammount.ToString();
+            A1IntervalTextBox.Text = A1Interval.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            cash += (int)Math.Pow(10, buttonLevel-1);
-            label1.Text = "Kasa: $"+ cash.ToString();
+            Cash += (int)Math.Pow(10, buttonLevel-1);
+            //label1.Text = "Kasa: $"+ cash.ToString();
         }
 
         private void upgradeButton_Click(object sender, EventArgs e)
         {
             int upgradeCost = (int)Math.Pow(10, buttonLevel);
-            if(cash >= upgradeCost)
+            if(Cash >= upgradeCost)
             {
                 buttonLevel++;
                 ButtonLevelTextBox.Text = buttonLevel.ToString();
-                cash -= upgradeCost;
-                label1.Text = "Kasa: $" + cash.ToString();
+                Cash -= upgradeCost;
+                //label1.Text = "Kasa: $" + cash.ToString();
                 string nextUpgradeCost = "($" + Math.Pow(10, buttonLevel).ToString() + ")";
                 upgradeButton.Text = "Upgrade\n" + nextUpgradeCost;
             }
@@ -33,6 +52,52 @@ namespace zadanko
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void A1UpgradeInterval_Click(object sender, EventArgs e)
+        {
+            int upgradeCost = A1Interval * 100;
+            if(Cash >= upgradeCost) {
+                A1Interval++;
+                A1IntervalTextBox.Text = A1Interval.ToString();
+                A1Timer.Interval = (60 / A1Interval) * 100;
+                if (!A1Timer.Enabled)
+                    A1Timer.Enabled = true;
+                Cash -= upgradeCost;
+                //label1.Text = "Kasa: $" + cash.ToString();
+            }
+        }
+
+        private void A1LevelTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void A1Timer_Tick(object sender, EventArgs e)
+        {
+            Cash += A1Ammount;
+            //label1.Text = "Kasa: $" + cash.ToString();
+        }
+
+        private void A1IntervalTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void A1UpgradeAmmount_Click(object sender, EventArgs e)
+        {
+            A1Ammount += 10;
+            A1ammountTextBox.Text = A1Ammount.ToString();
         }
     }
 }
